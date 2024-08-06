1c56cbbd3ddf3692b568c214d60283a3f4bc5b0e
dlogtime <43829050+dlogtime@users.noreply.github.com>
2024-08-06 22:45:59 +1000

    make the arena button load the ending dungeon

--- src/main/java/com/cocobuttgames/slaythearena/dungeons/Arena.java
+++ src/main/java/com/cocobuttgames/slaythearena/dungeons/Arena.java
@@ -1,31 +1,32 @@
-package com.cocobuttgames.slaythearena.dungeons;
+// package com.cocobuttgames.slaythearena.dungeons;
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+// import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
-public class Arena extends AbstractDungeon {
+// public class Arena extends AbstractDungeon {
+//     public Arena
+    
+//     protected void initializeLevelSpecificChances() {}
 
-    protected void initializeLevelSpecificChances() {}
+//     protected ArrayList<String> generateExclusions() {
+//         return new ArrayList<>();
+//     }
 
-    protected ArrayList<String> generateExclusions() {
-        return new ArrayList<>();
-    }
+//     protected void generateMonsters();
+    
+//     protected void generateWeakEnemies(int paramInt) {}
+    
+//     protected void generateStrongEnemies(int paramInt) {}
+    
+//     protected void generateElites(int paramInt) {}
+    
+//     protected void initializeBoss() {}
+    
+//     protected void initializeEventList() {}
+    
+//     protected void initializeEventImg() {}
+    
+//     protected void initializeShrineList() {}
 
-    protected void generateMonsters();
-    
-    protected void generateWeakEnemies(int paramInt) {}
-    
-    protected void generateStrongEnemies(int paramInt) {}
-    
-    protected void generateElites(int paramInt) {}
-    
-    protected void initializeBoss() {}
-    
-    protected void initializeEventList() {}
-    
-    protected void initializeEventImg() {}
-    
-    protected void initializeShrineList() {}
-
-}
+// }
--- src/main/java/com/cocobuttgames/slaythearena/patches/screens/mainMenu/MenuButtonPatch.java
+++ src/main/java/com/cocobuttgames/slaythearena/patches/screens/mainMenu/MenuButtonPatch.java
@@ -1,12 +1,24 @@
 package com.cocobuttgames.slaythearena.patches.screens.mainMenu;
 
+import com.cocobuttgames.slaythearena.patches.screens.mainMenu.MenuButtonPatch.ArenaClickResultPatch;
 import com.evacipated.cardcrawl.modthespire.lib.ByRef;
 import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.characters.CharacterManager;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.ModHelper;
+import com.megacrit.cardcrawl.helpers.SeedHelper;
+import com.megacrit.cardcrawl.map.MapRoomNode;
+import com.megacrit.cardcrawl.random.Random;
+import com.megacrit.cardcrawl.rooms.EmptyRoom;
+import com.megacrit.cardcrawl.screens.DungeonTransitionScreen;
+import com.megacrit.cardcrawl.screens.mainMenu.MainMenuScreen;
 import com.megacrit.cardcrawl.screens.mainMenu.MenuButton;
 import com.megacrit.cardcrawl.screens.mainMenu.MenuPanelScreen;
 
@@ -33,10 +45,34 @@ public class MenuButtonPatch {
         @SpirePrefixPatch
         public static SpireReturn<Void> setArenaButtonEffect(MenuButton __instance) {
             if (__instance.result == ArenaClickResultPatch.ARENA) {
-                CardCrawlGame.mainMenuScreen.panelScreen.open(MenuPanelScreen.PanelScreen.COMPENDIUM);
+                startArena();
             }
             return SpireReturn.Continue();
         }
+    }
+
+    public static void startArena() {
+        CardCrawlGame.mainMenuScreen.screen = MainMenuScreen.CurScreen.NONE;
+        CardCrawlGame.mainMenuScreen.hideMenuButtons();
+        CardCrawlGame.mainMenuScreen.darken();
+        CardCrawlGame.chosenCharacter = AbstractPlayer.PlayerClass.IRONCLAD;
+        CardCrawlGame.mainMenuScreen.isFadingOut = true;
+        CardCrawlGame.mainMenuScreen.fadeOutMusic();
+        CardCrawlGame.nextDungeon = "TheEnding";     // TODO: Make this load an Arena Dungeon instead
+        CardCrawlGame.dungeonTransitionScreen = new DungeonTransitionScreen("TheEnding");
+        CardCrawlGame.mode = CardCrawlGame.GameMode.GAMEPLAY;
+        Settings.isTrial = true;
+        Settings.isDailyRun = false;
+        Settings.isEndless = false;
+        AbstractDungeon.isAscensionMode = false;
+        AbstractDungeon.ascensionLevel = 0;
+        AbstractDungeon.player = CardCrawlGame.characterManager.recreateCharacter(AbstractPlayer.PlayerClass.IRONCLAD);
+        AbstractDungeon.currMapNode = new MapRoomNode(0, -1);
+        AbstractDungeon.currMapNode.room = new EmptyRoom();
+        long sourceTime = System.nanoTime();
+        Random rng = new Random(Long.valueOf(sourceTime));
+        Settings.seed = Long.valueOf(SeedHelper.generateUnoffensiveSeed(rng));
+        AbstractDungeon.generateSeeds();
     }
 
 }
